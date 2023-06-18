fcd0d23ee31323c082ef48625d25fcc8d9e40e07
Vladi <vladikarasove@gmail.com>
2023-06-18 15:20:26 +0300

    Initial Commit

---
+++ Twitter_IOS/Twitter_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_IOSApp.swift
+//  Twitter_IOS
+//
+//  Created by Student26 on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Twitter_IOSUITests/Twitter_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Twitter_IOSUITestsLaunchTests.swift
+//  Twitter_IOSUITests
+//
+//  Created by Student26 on 18/06/2023.
+//
+
+import XCTest
+
+final class Twitter_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
